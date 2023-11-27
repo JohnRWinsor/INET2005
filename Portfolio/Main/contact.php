ff4bc40315ca16dd861a563f88dae83fc389895b
John W <114405644+JohnRWinsor@users.noreply.github.com>
2023-11-27 15:10:50 -0400

    Update contact.php

--- Portfolio/Main/contact.php
+++ Portfolio/Main/contact.php
@@ -13,7 +13,23 @@ $pageName = 'Contact';
 <?php include './sections/header.php' ?>
 <?php include './sections/nav.php' ?>
     <h2><?php echo $pageName; ?></h2>
-    <p>Email: johnrwinsor@gmail.com</p>
+    
+    <header>
+        <h1>Contact Me</h1>
+    </header>
+     <form action="process_contact.php" method="post">
+            <label for="name">Your Name:</label>
+            <input type="text" id="name" name="name" required>
+
+            <label for="email">Your Email:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="message">Your Message:</label>
+            <textarea id="message" name="message" rows="4" required></textarea>
+
+            <button type="submit">Submit</button>
+        
+        </form>
     <p>School Email: w0474402@nscc.ca</p>
     <p>GitHub: <a href="https://github.com/JohnRWinsor">JohnRWinsor</a></p>
     <p>LinkedIn: <a href="https://www.linkedin.com/in/john-winsor-b1067b252/">John Winsor</a></p>
@@ -21,3 +37,10 @@ $pageName = 'Contact';
 </body>
 <?php include './sections/footer.php' ?>
 </html>
+
+<?php
+$activePage = 'contact';
+$pageName = 'My Contact Page';
+include("conn.php");
+?>
+
