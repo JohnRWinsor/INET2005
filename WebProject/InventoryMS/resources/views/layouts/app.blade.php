2421826051330b7b2f83710c5c7752f138011eef
John W <w0474402@campus.nscc.ca>
2023-12-05 13:14:55 -0400

    CSS Updates

--- WebProject/InventoryMS/resources/views/layouts/app.blade.php
+++ WebProject/InventoryMS/resources/views/layouts/app.blade.php
@@ -1,9 +1,77 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>App Name - @yield('title')</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>SuperHero Weapons</title>
+
+    <!-- Add your stylesheets, scripts, etc. here -->
+    <link rel='stylesheet' type='text/css' href='{{ asset('css/app.css') }}'>
+    <!-- Add any additional stylesheets or scripts here -->
+
+    <style>
+        /* Add custom styles for the header and navigation bar */
+        header {
+            background-color: #333;
+            padding: 10px;
+            color: white;
+            text-align: center;
+        }
+
+        nav {
+            display: flex;
+            justify-content: space-between;
+            background-color: #444;
+            padding: 10px;
+        }
+
+        nav a {
+            color: white;
+            text-decoration: none;
+            padding: 10px;
+        }
+
+        nav a:hover {
+            background-color: #555;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        table, th, td {
+            border: 1px solid #ddd;
+        }
+
+        th, td {
+            padding: 12px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        tr:hover {
+            background-color: #f5f5f5;
+        }
+    </style>
 </head>
 <body>
-    @yield('content')
+    <header>
+        <h1>SuperHero Weapons</h1>
+    </header>
+
+    <nav>
+        <a href="{{ url('/categories') }}">Categories</a>
+        <a href="{{ url('/items') }}">Items</a>
+    </nav>
+
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
